f900edae1edd62c114c2e321a36bede2b2221c3b
Eda Toktogulova <edatoktogulova2003@gmail.com>
2021-12-27 14:12:11 +0600

    added function

---
+++ script.js
@@ -0,0 +1,14 @@
+// Very simple function
+
+// playCards
+// cookDinner
+// finishmathHomework
+
+function callFreand () {
+    console.log('Pick up the phone');
+    console.log('Type in the number');
+    console.log('Press call');
+    console.log('Wait for the answer');
+}
+
+callFreand();
